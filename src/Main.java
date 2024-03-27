5f6bf59c54c8ed240aaeb0c2c1bf1b309d762469
Encashment <burdak0101@list.ru>
2024-03-27 12:41:07 +0300

    change5

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
         System.out.println("Hello world \n" +
                 "Hello world\n" +
                 "Hello world\n" +
+                "Hello world\n" +
                 "Hello world!");
     }
 }
